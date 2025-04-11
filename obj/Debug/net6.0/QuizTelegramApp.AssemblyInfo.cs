25d31222a0eda5019a518194bc303ab617930ba9
DrRexar <melenzozeon@mail.ru>
2025-04-11 13:08:02 +0700

    Switch to Dockerfile for deployment

--- obj/Debug/net6.0/QuizTelegramApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/QuizTelegramApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizTelegramApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+171bc20e80156ac3cf2c306a801a198889092dd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d497f2e5a8419218b4846bca3afd4e189d5632ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizTelegramApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizTelegramApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
