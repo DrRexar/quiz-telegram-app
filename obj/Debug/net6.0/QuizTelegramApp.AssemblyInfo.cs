ea90ad13bc28aacf6190f967502f3483f0e95798
DrRexar <melenzozeon@mail.ru>
2025-04-11 12:37:24 +0700

    Fix Blazor components for .NET 6.0

--- obj/Debug/net6.0/QuizTelegramApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/QuizTelegramApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizTelegramApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495906a59a5424ffcf3651f73f996b1d90d83d46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+171bc20e80156ac3cf2c306a801a198889092dd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizTelegramApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizTelegramApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
